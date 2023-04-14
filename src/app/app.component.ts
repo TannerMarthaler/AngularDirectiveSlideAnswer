9829f3f9b1b3c2c9f8eb78d3df84ab477c7ae25d
TannerMarthaler <125493123+TannerMarthaler@users.noreply.github.com>
2023-04-14 11:30:15 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  toggle: boolean = true;
+
+  count: number = 0;
+  logOfCount: number[] = [];
+
+  changeToggle(): void {
+    this.toggle = !this.toggle;
+    this.logOfCount.push(this.count++);
+  }
+
+  clearLog(): void {
+    this.count = 0;
+    this.logOfCount = [];
+  }
+}
